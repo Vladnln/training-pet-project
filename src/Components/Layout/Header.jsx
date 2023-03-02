c03533493d5a97b322e06322744489d13fd8991a
Vlad <vlad-golubev-2019@list.ru>
2023-03-02 12:19:46 +0300

    first

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import '../../App.css'
+import Logo from "../../assets/img/Logo.png"
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header__wrapper">
+                <div className="header">
+                    <div className="HeaderLogo">
+                        <img src={Logo} alt=""/>
+                    </div>
+                    <div className="header__links">
+                        <a href="#">Home</a>
+                        <a href="#">Features</a>
+                        <a href="#">Testimonial</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">CTA</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Header from "../Components/Layout/Header.jsx";
+import '../App.css'
+class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default MainPage;
