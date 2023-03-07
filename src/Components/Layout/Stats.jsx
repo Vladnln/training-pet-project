111a5c2d79f679ab1d001ab0fac3b90430eadc81
Vlad <vlad-golubev-2019@list.ru>
2023-03-07 10:42:40 +0300

    mark descr

--- src/Components/Layout/Stats.jsx
+++ src/Components/Layout/Stats.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Group from '..//..//assets/img/Group487.png'
+import Icon1 from '..//..//assets/icons/Group383.png'
+import Icon2 from '..//..//assets/icons/Group384.png'
 class Stats extends Component {
     render() {
         return (
@@ -11,7 +13,12 @@ class Stats extends Component {
                     <div className="stats__description">
                         <p className="title">lorem ipsum dolor</p>
                         <h1 className="main__title stats__main__title"><span>Two</span> Saas are better <span>than one.</span> </h1>
-                        <p className="main__subtitle main__stats__subtitle">One of the most fundamental ways to go about that is to create a memorable e-commerce slogan.</p>
+                        <p className="main__subtitle main__stats__subtitle">One of the most fundamental ways to go about that is to create a memorable <span>e-commerce</span> slogan.</p>
+                        <div className="stats__marks">
+                            <li><img src={Icon1} alt=""/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+                            <li><img src={Icon2} alt=""/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+                            <li><img src={Icon1} alt=""/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</li>
+                        </div>
                     </div>
                 </div>
 
