0604150fcc462429b77d2a31c193f1251546e236
Vlad <vlad-golubev-2019@list.ru>
2023-03-15 16:00:52 +0300

    all cards completed

--- src/Components/Layout/SubscriptionCards.jsx
+++ src/Components/Layout/SubscriptionCards.jsx
@@ -30,6 +30,58 @@ class SubscriptionCards extends Component {
                     </div>
                     <button className="graphics__buttons button__order">Buy Now</button>
                 </div>
+                <div className="subscription__card-wrapper">
+                    <h1 className="card__title subscription__card-title">PRO</h1>
+                    <p className="card__description subscription__card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+                    <h1 className="subscription__card-price">$30</h1>
+                    <div>
+                        <button className="subscription__buttons">
+                            <button className="subscription__button">Monthiy</button>
+                            <button className="subscription__button">Annually</button>
+                        </button>
+                    </div>
+                    <div className="subscription__mark-list">
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Consulting with experts 20 times for I yea</p>
+                        </div>
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Add friends for free for 1month</p>
+                        </div>
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Free 5 fonts</p>
+                        </div>
+                    </div>
+                    <button className="graphics__buttons button__order">Buy Now</button>
+                </div>
+                <div className="subscription__card-wrapper">
+                    <h1 className="card__title subscription__card-title">COMPANY</h1>
+                    <p className="card__description subscription__card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+                    <h1 className="subscription__card-price">$70</h1>
+                    <div>
+                        <button className="subscription__buttons">
+                            <button className="subscription__button">Monthiy</button>
+                            <button className="subscription__button">Annually</button>
+                        </button>
+                    </div>
+                    <div className="subscription__mark-list">
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Consulting with experts 20 times for I yea</p>
+                        </div>
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Add friends for free for 1month</p>
+                        </div>
+                        <div className="subscription__mark-list check-list">
+                            <img src={Checkmark} alt="" />
+                            <p className="subscription__mark-list list-title">Free 5 fonts</p>
+                        </div>
+                    </div>
+                    <button className="graphics__buttons button__order">Buy Now</button>
+                </div>
             </div>
         );
     }
