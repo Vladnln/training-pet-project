9ad21c017f4c2bd476d1af8a258dea59f1c2138c
Vlad <vlad-golubev-2019@list.ru>
2023-03-30 17:10:58 +0300

    footer

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import Logo from "../../assets/img/Logo.png";
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <div className="footer__container"></div>
-
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="footer">
+				<div className="footer__container">
+					<img src={Logo} alt="" />
+					<p className="footer__title">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Footer;
